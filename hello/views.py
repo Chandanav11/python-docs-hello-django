9a25ff0336a0059d96b84153e95704fcb89815d7
Chandanav11 <107384406+Chandanav11@users.noreply.github.com>
2022-06-13 10:53:02 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello team , happy weekend")
+    return HttpResponse("Hello Chandana, Good Morning! We are learning Azure And we are Happy")
